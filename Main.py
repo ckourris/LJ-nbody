1149a1d29f678fceea30f3d643664a7955d5b8c7
s1612285@sms.ed.ac.uk <christos.kourris.cut@gmail.com>
2019-02-01 11:08:18 +0000

    Corrected arguments input

--- Main.py
+++ Main.py
@@ -10,15 +10,8 @@ from Utilities import *
 from MDUtilities import *
 
 def main(arg1, arg2):
-    if len(sys.argv)!= 3:
-        print("Wrong number of arguments.")
-        print("Usage: " + sys.argv[0] + " <output file>")
-        quit()
-    else:
-        param_name = sys.argv[1]
-        out_name = sys.argv[2]
-
-    parameters, outfile = get_arguments(param_name, out_name)
+    # Read parameter and output file names from sys.argv
+    parameters, outfile = get_arguments()
 
     Box = Box(parameters[0], parameters[2], parameters[1], parameters[3])
 
