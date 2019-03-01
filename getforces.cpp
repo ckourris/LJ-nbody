1aa31599b3be26bec4fea921e360244227add622
s1612285@sms.ed.ac.uk <christos.kourris.cut@gmail.com>
2019-03-01 12:56:49 +0000

    Fixed minor error, now matches Python

--- getforces.cpp
+++ getforces.cpp
@@ -35,17 +35,17 @@ void addvector(double* invec, double* outvec, bool negative){
 void getforces(double* in_array, double* out_array, int N, double boxdim, double cutoff){
     //Set out ndarray to zero
     for(int i = 0; i<3*N; i++) out_array[i]=0;
-    
+
     // Setup temporary force vector;
     double forcevar[3];
-    
+
     // Calculate all forces by iterating over unique i, j pairs
     for(int i = 0; i<N; i++){
         for(int j = 0; j < i; j++){
             force(in_array+i*3, in_array+j*3, forcevar, boxdim, cutoff);
 //          cout << forcevar[0] << ' ' << forcevar[1] << endl;
-            addvector(forcevar, out_array+i*3, 0);
-            addvector(forcevar, out_array+j*3, 1);
+            addvector(forcevar, out_array+j*3, 0);
+            addvector(forcevar, out_array+i*3, 1);
         }
     }
 
